4454546e05a9b042a1ae64b79a6754ae367d40b8
backmeupplz <backmeupplz@gmail.com>
2020-04-19 09:42:07 -0700

    add disabling web preview

--- src/commands/help.ts
+++ src/commands/help.ts
@@ -3,7 +3,9 @@ import { Telegraf, ContextMessageUpdate } from 'telegraf'
 import { checkLock } from '../middlewares/checkLock'
 
 export function setupHelp(bot: Telegraf<ContextMessageUpdate>) {
-  bot.command(['help', 'start'], checkLock, async ctx => {
-    await ctx.replyWithHTML(ctx.i18n.t('help', { id: ctx.chat.id }))
+  bot.command(['help', 'start'], checkLock, async (ctx) => {
+    await ctx.replyWithHTML(ctx.i18n.t('help', { id: ctx.chat.id }), {
+      disable_web_page_preview: true,
+    })
   })
 }
